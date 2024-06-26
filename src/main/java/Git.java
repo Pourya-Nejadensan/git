e97e0e0d3653356a10cc00d97adb68e11e55651d
Pourya <pourya.nejadensan@gmail.com>
2024-06-26 14:48:10 +0200

    Add sout Java

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -2,5 +2,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("Git");
         System.out.println("Version Konrolle");
+        System.out.println("Java");
     }
 }
